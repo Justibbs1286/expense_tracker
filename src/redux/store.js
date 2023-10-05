d87cea2f23b222d4461ef88746b8d3a4a0795abf
Ibrahim Indorewala <ibrahim.indorewala.btech2021@sitpune.edu.in>
2023-10-05 12:13:48 +0530

    e

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
-import { createStore } from 'redux'
+//import { createStore } from 'redux'
+import { legacy_createStore as createStore} from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
